24046d09acced2d618a6b5f8bf9beec6005b0d43
Mohamad Rais <rais.sahri@gmail.com>
2019-11-29 21:14:19 +0800

    Dummy page created

--- pgs66/header.php
+++ pgs66/header.php
@@ -88,7 +88,7 @@ $self_baseurl = _baseurl();
 		<script src="<?php echo PREPEND_PATH; ?>dist/metisMenu/metisMenu.js"></script>
 		<script src="<?php echo PREPEND_PATH; ?>dist/js/sb-admin-2.js"></script>
 		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>../assets/plugins/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>assets/css/style.css">
+		<!-- <link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>assets/css/style.css"> -->
 		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>../assets/css/style.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.8.1/js/perfect-scrollbar.jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.8.1/js/perfect-scrollbar.min.js"></script>
