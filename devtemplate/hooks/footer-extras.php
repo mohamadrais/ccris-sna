f660af481407e7b3357759034c757b5a6ac3a59b
shakucing <aishah.fr@gmail.com>
2020-01-14 01:17:08 +0800

    fix for save mode in detail view

--- devtemplate/hooks/footer-extras.php
+++ devtemplate/hooks/footer-extras.php
@@ -12,192 +12,32 @@ if ($memberInfo['group'] != 'Admins') {
 }
 ?>
 <script type='text/javascript'>
+    $j('#attach').on('click touchstart', function(e) {
+        // e.preventDefault(); 
+        // e.stopPropagation();
+        var tableName = $j('[name ="myform"]').attr('action');
+        tableName = tableName.substr(0, tableName.indexOf('_view.php'));
+        var selectedID = $j("[aria-labelledby='selectedID']").html().trim();
+        get_workorders(tableName, selectedID);
+        $j('div[id$="dv_action_buttons"]').append(`
+            <div class="modal" id="wo_modal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="card-title">Select a work order</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="message-box" style="max-height: 358px;overflow: scroll;">
+                                <ul class="list-task todo-list list-group m-b-0" id="wo_content_list"><span id="wo_content"></span></ul>
+                            </div>
+                        </div>
+                        <div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
     document.observe("dom:loaded", function() {
-		if($j('#notif-dropdown').is(':visible')){
-			$j('#notif-dropdown').on('click touchstart', function(e) {
-				e.preventDefault(); 
-				// e.stopPropagation();
-				$j.ajax({
-					url: "ajax_crud_noti.php",
-					method: "POST",
-					data: {
-						'o'	 : 'select',
-						'nls': '0',
-						'nl' : '20'
-					},
-					dataType: "JSON",
-					success: function(notifData) {
-						// var keys = Object.keys(notifData[0]);
-						// notiArr = [];
-						// for (i=0; i< keys.length; i++){
-						// 	_dataColumnNames.push(keys[i].replace(/_/g, ' '));
-						// }
-						// notiArr.push(_dataColumnNames);
-						$j('#notif-dropdown-area').empty();
-						let unreadCount = 0;
-						if(notifData.length > 0){
-							let _notifications=[];
-							let day = moment($j.ajax({async: false}).getResponseHeader( 'Date' )).tz("Asia/Kuala_Lumpur");
-							for(x=0; x< notifData.length; x++){
-								let currNotifLine = '';
-								notifIconClass = '', readFlagClass = '';
-								switch(notifData[x]['notif_title']){
-									case 'New work order':
-										notifIconClass = 'class="fa fa-tasks pull-right text-info"';
-										break;
-									case 'Update on work order':
-										notifIconClass = 'class="fa fa-paperclip fa-pull-right text-warning"';
-										break;
-									case 'KPI not achieved':
-										notifIconClass = 'class="fa fa-exclamation-circle fa-pull-right text-danger"';
-										break;
-								}
-								if (notifData[x]['read_flag'] == 'N'){
-									unreadCount++;
-									readFlagClass = 'class="new"';
-									btnReadText = 'Mark as Read';
-								} 
-								else{
-									btnReadText = 'Mark as Unread';
-								}
-								let notif_time = moment(notifData[x]['notif_time']);
-								if(moment(notif_time).isSame(day, 'day')){
-									notif_time = notif_time.format('hh:mm A');
-								}
-								else {
-									notif_time = moment(notif_time).fromNow();
-								}
-								currNotifLine += `
-									
-									<a id="notif_id_${notifData[x]['id']}" ${readFlagClass} href="${notifData[x]['notif_url']}">
-										<div class="mail-contnet">
-											<i ${notifIconClass}></i>
-											<h5>${notifData[x]['notif_title']}</h5><span class="mail-desc">${notifData[x]['notif_msg']}<span class="time">${notif_time}</span> </div>
-										</span>
-									</a>`;
-								_notifications.push(currNotifLine);
-							}
-							if(unreadCount > 0){
-								if(!$j('#notif-heartbit').hasClass('hearbit')) $j('#notif-heartbit').addClass('heartbit');
-								if(!$j('#notif-point').hasClass('point')) $j('#notif-point').addClass('point');		
-							}
-							$j('#notif-dropdown-area').prepend(_notifications);
-						}
-						else{
-							$j('#notif-dropdown-area').prepend(`
-								<a href="#">
-									<div class="mail-contnet">
-										<h5>You do not have any active notifications</h5> <span class="mail-desc"></span> <span class="time"></span> </div>
-								</a>
-							`);
-							$j('#mark-all-read').hide();
-							$j('#see-all-notif').hide();
-						}
-						
-					},         
-					error: function(response) {
-						console.log('notif-dropdown ajax error: ' + response.statusText);
-					},
-				});
-			});
-			$j('#notif-dropdown-area').on("click touchstart", "a[id^='notif_id_']" , function(e) {
-				e.preventDefault();
-				// e.stopPropagation();
-				
-				var nId = $j(this).attr('id').substr(9, $j(this).attr('id').length-1);
-				if($j(this).hasClass('new')){
-					$j.ajax({
-						url: "ajax_crud_noti.php",
-						method: "POST",
-						data: {
-							'o'  : 'toggle_r',
-							'nId': nId
-						},
-						dataType: "JSON",
-						success: function(read_flag_returned) {
-							// window.location = $link.attr('href');
-						},         
-						error: function(response) {
-							console.log('notif-dropdown-area-a ajax error: ' + response.statusText);
-						},
-					});
-				}
-				var unreadCount = 0;
-				$j(this).parent().find('a').each(function(i, el){
-					if($j(el).hasClass('new')) unreadCount++;
-				});
-				if(unreadCount <= 1){
-					if($j('#notif-heartbit').hasClass('heartbit')) $j('#notif-heartbit').removeClass('heartbit');
-					if($j('#notif-point').hasClass('point')) $j('#notif-point').removeClass('point');
-				}
-				window.location = $j(this).attr('href');
-			});
-			$j('#mark-all-read').on("click touchstart", function(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				$j('#notif-dropdown-area a').each(function(i, el){
-					var nId = $j(el).attr('id').substr(9, $j(el).attr('id').length-1);
-					if($j(this).hasClass('new')){
-						$j.ajax({
-							url: "ajax_crud_noti.php",
-							method: "POST",
-							data: {
-								'o'  : 'toggle_r',
-								'nId': nId
-							},
-							dataType: "JSON",
-							success: function(read_flag_returned) {
-								$j(el).removeClass('new');
-							},         
-							error: function(response) {
-								console.log('notif-dropdown-mark-all ajax error: ' + response.statusText);
-							},
-						});
-					}
-				});
-				if($j('#notif-heartbit').hasClass('heartbit')) $j('#notif-heartbit').removeClass('heartbit');
-				if($j('#notif-point').hasClass('point')) $j('#notif-point').removeClass('point');
-			});
-		}
-		
-		
-		// if($j('#attach').is(':visible')){
-			$j('#attach').on('click touchstart', function(e) {
-				// e.preventDefault(); 
-				// e.stopPropagation();
-				var tableName = $j('[name ="myform"]').attr('action');
-				tableName = tableName.substr(0, tableName.indexOf('_view.php'));
-				var selectedID = $j("[aria-labelledby='selectedID']").html().trim();
-				var uniqID = uniqId();
-				$j('[name ="myform"]').append(`
-					<div class="modal" id="wo_modal">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h4 class="card-title">Select a work order</h4>
-								</div>
-								<div class="modal-body">
-									<div class="message-box" style="max-height: 358px;overflow: scroll;">
-										<ul class="list-task todo-list list-group m-b-0" id="wo_content_list">
-											<span id="wo_content-container${uniqID}"></span>
-											<input type="hidden" name="wo_content-input" id="wo_content-input${uniqID}" value="">
-										</ul>
-									</div>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-primary" id="attachWOButton" disabled>Attach</button>
-									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				`);
-				get_workorders(tableName, selectedID, uniqID);
-			});
-		// }
-		function uniqId() {
-			return Math.round(new Date().getTime() + (Math.random() * 100));
-		}
 		if(!$j('#searchPageForm').is(':visible')){
 			$j('#fullsearchicon').parent().replaceWith(`
 				<form role="search" method="get" class="search-form" action="search.php" id="searchForm">
@@ -327,24 +167,19 @@ if ($memberInfo['group'] != 'Admins') {
                 });
 
 				$j(this).hide();
-				$j("#exitEdit").show();
+				$j("#updateRecord").show();
 			});
 			
-			$j("#exitEdit").click(function(){
-				$j("input").attr("readonly", true);
-				$j("textarea").attr("readonly", true);
-                $j("select").attr("disabled", true);
-                $j('input[type="radio"]').attr("disabled", true);
-				$j(".nicEdit-main").attr("contenteditable", "false");
-				$j('#ot_SharedLink1_remove').hide();
-                $j('#ot_SharedLink2_remove').hide();
-                $j('#addLink').hide();
-                $j('#addDocument').hide();
-                $j('#addCompFolder').hide();
-                $j('#addPhoto').hide();
-				$j(this).hide();
-				$j("#startEdit").show();
-            });
+			// $j("#cancelEdit").click(function(){
+			// 	if(!$j("#startEdit").is(":visible")){
+			// 		var tableName = $j('[name ="myform"]').attr('action');
+			// 		// tableName = tableName.substr(0, tableName.indexOf('_view.php'));
+			// 		window.location.href="./"+tableName+"?SelectedID="+$j("[aria-labelledby='selectedID']").html().trim();
+			// 	}
+			// 	else{
+			// 		window.location.href="./"+tableName+"?SelectedID="+$j("[aria-labelledby='selectedID']").html().trim();
+			// 	}
+            // });
         }
             
             ///////////////////////////////////////////////
